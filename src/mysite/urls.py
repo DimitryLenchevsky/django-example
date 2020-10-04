af5fd2268d18541110745f5f7a09b1a8bd37cf1c
DimitryLenchevsky <overborsch@gmail.com>
2020-10-04 13:56:27 +0300

    some syntax errors in urls are fixed

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from webexample.views import index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webexample', index)
+    path('webexample', include('webexample.urls'))
 
 ]
