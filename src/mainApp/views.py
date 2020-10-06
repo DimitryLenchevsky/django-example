16bb9c3c817078f13943c59a25bbc69638cf8e71
DimitryLenchevsky <overborsch@gmail.com>
2020-10-06 19:47:39 +0300

    new features

--- src/mainApp/views.py
+++ src/mainApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'mainApp/homePage.html')
+
+def contact(request):
+    return render(request, 'mainApp/basic.html', {'content' : ['Если у вас остались вопросы, задавайте их по телефонц', '+375 (25) 714 85 68']})
