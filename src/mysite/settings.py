e6e0b2832806e4b3ba98908c79f200bd4bd1816d
DimitryLenchevsky <overborsch@gmail.com>
2020-10-07 16:30:43 +0300

    models

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webexample',
-    'mainApp'
+    'mainApp',
+    'news'
 ]
 
 MIDDLEWARE = [
--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('webexample/', include('webexample.urls')),
-    path('', include('mainApp.urls'))
+    path('', include('mainApp.urls')),
+    path('news', include('news.urls'))
 
 ]
