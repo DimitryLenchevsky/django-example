f8f1ddb343eeecc2efe3f66c2bd8229fc1097f8a
DimitryLenchevsky <overborsch@gmail.com>
2020-10-29 16:29:00 +0300

    views ase done

--- src/main/views.py
+++ src/main/views.py
@@ -3,7 +3,7 @@ from .models import Task
 
 
 def index(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.order_by('-id')[:5]
     return render(request, 'main/index.html', {'title' : 'Главная страница сайта', 'tasks' : tasks})
 
 def about(request):
