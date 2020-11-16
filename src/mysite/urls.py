a4f80aec6ab2e6b4600b00eced98a9bc9b8da4d3
DimitryLenchevsky <overborsch@gmail.com>
2020-11-16 15:18:46 +0300

    api-auth is getting ready

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 
@@ -24,4 +26,7 @@ urlpatterns = [
     path('', include('main.urls')),
     path('api-auth/', include('rest_framework.urls'))
 
-]
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
