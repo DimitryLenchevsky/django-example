2dc530a1f2b4ca7016ad2058ba9b79dad0c6bee6
DimitryLenchevsky <overborsch@gmail.com>
2020-11-19 17:23:48 +0300

    permissions

--- src/cars/views.py
+++ src/cars/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import  CarsListSerializer, CarDetailSerializer
 from cars.models import Car
+from cars.permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class CarCreateView(generics.CreateAPIView):
@@ -11,8 +13,10 @@ class CarCreateView(generics.CreateAPIView):
 class CarsListView(generics.ListAPIView):
     serializer_class = CarsListSerializer
     queryset = Car.objects.all()
+    permission_classes = (IsAuthenticated, IsAdminUser)
 
 
 class CarDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CarDetailSerializer
     queryset = Car.objects.all()
+    permission_classes = (IsOwnerOrReadOnly, )
