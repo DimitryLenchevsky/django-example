31668462389dc9cb35081d37da9aaca547ff61ac
DimitryLenchevsky <overborsch@gmail.com>
2020-11-19 11:57:58 +0300

    HiddenField bug fix

--- src/cars/serializers.py
+++ src/cars/serializers.py
@@ -3,12 +3,15 @@ from .models import Car
 
 
 class CarDetailSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Car
         fields = '__all__'
 
 
 class CarsListSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Car
         fields = ('id', 'vin', 'user')
